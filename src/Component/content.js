ad36b87790bbef1ccc4b276d721e662436b85296
jeyaprakash0412 <jpwin1997@gmail.com>
2021-07-05 16:04:52 +0530

    next

--- src/Component/content.js
+++ src/Component/content.js
@@ -9,8 +9,68 @@ const Content = () => {
             <Second/>
             <div class="mt-5">
             <Navbar/>
-            
+            <div class="row text-white bg-dark">
+    <div class="col-2 " align="left" >
+      Menu
+    
 
+    <div class="col "align="center">
+    Dashboards
+    </div>
+    <br></br>
+    
+    <div class="col"align="center">
+    Default
+    </div>
+    <br></br>
+    
+    <div class="col"align="center">
+    Saas
+    </div>
+    <br></br>
+    <div class="col"align="center">
+    Crypto
+    </div>
+    <br></br>
+    <div class="col"align="center">
+    Blog
+    </div>
+    <br></br>
+    <div class="col"align="Left">
+    Apps
+    
+    <div class="col "align="center">
+    Calendar
+    </div>
+    <br></br>
+    
+    <div class="col"align="center">
+    Chat
+    </div>
+    <br></br>
+    
+    <div class="col"align="center">
+    File Manager
+    </div>
+    <br></br>
+    <div class="col"align="center">
+    Ecommerce
+    </div>
+    <br></br>
+    <div class="col"align="center">
+    Crypto
+    </div>
+    <br></br>
+    <div class="col"align="center">
+    Email
+    </div>
+
+    
+    
+
+    </div>
+    </div>
+    </div>
             </div>
             
         </div>
